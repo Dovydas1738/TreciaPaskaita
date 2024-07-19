e419a72b6e090b276469270cfcad431f9964d8bb
Dovydas Montvydas <dovism@gmail.com>
2024-07-19 15:15:24 +0300

    Add project files.

---
+++ TreciaPaskaita/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+
+namespace ManoPrograma
+{
+    public class PagrindineKlase
+    {
+        public static void Main(string[] args)
+        {
+            /*int[] masyvas1 = { 1, 2, 3, 4, 5 };
+            int[] masyvas2 = { 6, 222, 3, 8, 543, 2, 90, 17, 84 };
+            int[] masyvas3 = { 0, 0, 0, 0, 5, 0, 0, 0, 0 };
+
+            // 1 uzduotis
+
+            Console.WriteLine("Pirmas masyvu elementas yra: " + masyvas1[0] + " " + masyvas2[0] + " " + masyvas3[0]);
+
+            // 2 uzduotis (BUTINA ISKOMENTUOT nes gadina 7 uzduoti)
+
+            //masyvas1[1] = 70;
+
+            Console.WriteLine("Pakeistas antras elementas: " + masyvas1[1]);
+
+            // 3 uzduotis
+
+            Console.WriteLine("Masyvo ilgis yra: " + masyvas1.Length);
+
+            // 4 uzduotis
+
+            Console.WriteLine("Trecias elementas masyve yra: " + masyvas1[2]);
+
+            // 5 uzduotis
+
+            Console.WriteLine("Paskutinis masyvo elementas yra: " + masyvas1.Last());
+
+            // 6 uzduotis
+
+            //Console.WriteLine("Masyvo vidurio elementas yra: " + masyvas1[masyvas1.Length / 2]);
+
+            // 7 uzduotis
+
+             Console.WriteLine("Sudeti masyvo elementai: " + masyvas1.Sum());
+            */
+
+            // 8 uzduotis***************************************************************
+
+            //int[] pirmasMasyvas = { 1, 5, 6, 7, 8, 10, 15 };
+
+            //Console.WriteLine("Iveskite skaiciu");
+            //int ivestasSkaicius = int.Parse(Console.ReadLine());
+
+            //if (pirmasMasyvas.Contains(ivestasSkaicius))
+            //{
+            //    Console.WriteLine("Skaicius yra masyve");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Skaiciaus nera masyve");
+            //}
+
+            // 9 uzduotis**************************************************************
+
+            //double[] doubleMasyvas = { 2.55, 3, 5.89, 7, 10.1 };
+            //Console.WriteLine(doubleMasyvas[0] + " " + doubleMasyvas[1] + " " + doubleMasyvas[2] + " " + doubleMasyvas[3] + " " + doubleMasyvas[4]); 
+
+            //Console.WriteLine("Iveskite indeksa");
+            //int indeksas = int.Parse(Console.ReadLine());
+
+            //if (indeksas > doubleMasyvas.Length || indeksas < 0)
+            //{
+            //    Console.WriteLine("Indeksas negalioja");
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine("Iveskite nauja elementa siame indekse");
+            //    doubleMasyvas[indeksas] = double.Parse(Console.ReadLine());
+
+            //    Console.WriteLine("Naujas masyvas: " + doubleMasyvas[0] + " " + doubleMasyvas[1] + " " + doubleMasyvas[2] + " " + doubleMasyvas[3] + " " + doubleMasyvas[4]);
+            //}
+
+            // 10 uzduotis**************************************************************
+
+            //char[] arrayChar = { 'a', 'b', 'c', 'd', 'e' };
+
+            //Console.WriteLine("Iveskite raide");
+            //char raide = char.Parse(Console.ReadLine());
+
+
+            //if (arrayChar.Contains(char.ToLower(raide)))
+            //{
+            //    Console.WriteLine("Raide yra masyve");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Raides nera masyve");
+            //}
+
+            // 11 uzduotis**************************************************************
+
+            //string[] arrayString = { "labas", "ne labas", "o gal ir labas" };
+
+            //Console.WriteLine("Iveskite eilute");
+            //string ivestaEilute = Console.ReadLine();
+
+            //if (arrayString.Contains(ivestaEilute))
+            //{
+            //    Console.WriteLine("Eilute yra masyve");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Eilutes nera masyve");
+            //}
+
+            // 12 uzduotis***********************************************************
+
+            //float[] arrayFloats = { 5, 10, 11, 15, 20 };
+
+            //Console.WriteLine("Iveskite du skaicius");
+            //float pirmasSk = float.Parse(Console.ReadLine());
+            //float antrasSk = float.Parse(Console.ReadLine());
+
+            //if (arrayFloats.Contains(pirmasSk + antrasSk))
+            //{
+            //    Console.WriteLine("Suma atitinka masyvo elementa");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Suma neatitinka jokio masyvo elemento");
+            //}
+
+            // 13 uzduotis******************************************************** PAKLAUST CIA
+
+            //int[] arrayIntu = { 1, 5, 6, 8, 9, 10 };
+
+            //Console.WriteLine("Iveskite indeksa");
+            //int index = int.Parse(Console.ReadLine());
+
+            //if (index > arrayIntu.Length || index < 0)
+            //{
+            //    Console.WriteLine("Indeksas neegzistuoja");
+            //}
+
+            //else
+            //{
+            //    Console.WriteLine("Indeksas egzistuoja masyve");
+            //}
+
+
+
+        }
+    }
+}
